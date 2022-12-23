4f6bc40fffc23b62290153705a75910b5b333490
marco-mei <meinerzhagen.marco@gmail.com>
2022-12-23 17:02:41 +0100

    A

---
+++ Loops/Nested List Comprehension/task.py
@@ -0,0 +1,6 @@
+string = '0123456789'
+
+matrix = [[i for i in string]for i in string]
+
+for row in matrix:
+    print(row)
---
+++ Strings/F-strings/f_strings.py
@@ -0,0 +1,3 @@
+name = "Marco"
+age = 23
+print(f"Hello, My name is {name} and I'm {age} years old.")
